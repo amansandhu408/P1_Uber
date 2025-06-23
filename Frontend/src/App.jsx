00c65eb24010767340f2af2699668ddd0055e68e
AMAN PREET SINGH <amansandhu2241@gmail.com>
2025-06-24 02:25:01 +0530

    Created User and Captain UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,7 +11,10 @@ import UserProtectWrapper from './Pages/UserProtectWrapper';
 import UserLogout from './Pages/UserLogout';
 import CaptainHome from './Pages/CaptainHome';
 import CaptainProtectWrapper from './Pages/CaptainProtectWrapper';
-import CaptainLogout from './Pages/CaptainLogout';  
+import CaptainLogout from './Pages/CaptainLogout';
+import Riding from './Pages/Riding';  
+import CaptainRiding from './Pages/CaptainRiding';
+import 'remixicon/fonts/remixicon.css' 
 
 const App =()=>{
     return (
@@ -19,6 +22,8 @@ const App =()=>{
             <Routes>
                 <Route path='/' element={<Start/>} />
                 <Route path='/login' element={<UserLogin/>} />
+                <Route path='/riding' element={<Riding/>} />
+                <Route path='/captain-riding' element={<CaptainRiding/>} />
                 <Route path='/signup' element={<UserSignup/>} />
                 <Route path='/captain-login' element={<CaptainLogin/>} />      
                 <Route path='/captain-signup' element={<CaptainSignup/>} />
